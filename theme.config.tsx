c16ec6e3cbaa71f56c0a6ca5f172d8c93e4c099b
Benjamin <bfzli@hotmail.com>
2024-06-07 14:44:42 +0200

    @bfzli | Small Touches

--- theme.config.tsx
+++ theme.config.tsx
@@ -9,7 +9,9 @@ const config: DocsThemeConfig = {
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <link rel='icon' type='image/x-icon' href='/favicon.ico' />
 
-        <title>Goat Slider Documentation</title>
+        <title>
+            Goat Slider Documentation
+        </title>
 
         <meta
           name='description'
