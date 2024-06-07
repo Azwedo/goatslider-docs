51832c1d53948d0706a8155ee5bba3931968be32
Arno Fukuda <arnofkd@gmail.com>
2024-06-07 21:20:22 +0900

    fix: remove default head tags

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,19 +3,58 @@ import { LogoIcon } from "ui/icons"
 
 const config: DocsThemeConfig = {
   logo: <LogoIcon />,
+  head: function Head() {
+    return (
+      <>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <link rel='icon' type='image/x-icon' href='/favicon.ico' />
+
+        <title>Goat Slider Documentation</title>
+
+        <meta
+          name='description'
+          content='A detailed documentation for Goat Slider.'
+        />
+
+        <meta property='og:title' content='Goat Slider Documentation' />
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://docs.goatslider.com' />
+        <meta property='og:site_name' content='Goat Slider' />
+        <meta property='og:locale' content='en_US' />
+
+        <meta
+          property='og:image'
+          content='https://docs.goatslider.com/assets/og_thumbnail.jpg'
+        />
+        <meta
+          property='og:description'
+          content='A detailed documentation for Goat Slider.'
+        />
+
+        <meta
+          name='twitter:image'
+          content='https://docs.goatslider.com/assets/og_thumbnail.jpg'
+        />
+        <meta
+          name='twitter:description'
+          content='A detailed documentation for Goat Slider.'
+        />
+      </>
+    )
+  },
   useNextSeoProps() {
     return {
       titleTemplate: "%s | Goat Slider",
       defaultTitle: "Goat Slider Documentation",
       description: "A detailed documentation for Goat Slider.",
       keywords: [
-        "Goat Slider", 
-        "Documentation", 
+        "Goat Slider",
+        "Documentation",
         "Azwedo",
         "Goat Slider Docs",
         "Goat Slider Documentation",
-        "Learn Goat Slider"
-    ],
+        "Learn Goat Slider",
+      ],
       openGraph: {
         type: "website",
         locale: "en_US",
@@ -23,31 +62,32 @@ const config: DocsThemeConfig = {
         site_name: "Goat Slider",
         description: "A detailed documentation for Goat Slider.",
         images: [
-            {
-                url: '/assets/og_thumbnail.png'
-            }
-        ]
+          {
+            url: "/assets/og_thumbnail.png",
+          },
+        ],
       },
       twitter: {
-        card: 'summary_large_image',
-        site: '@goatslider',
-        title: 'Goat Slider Documentation',
-        description: 'A detailed documentation for Goat Slider.',
+        card: "summary_large_image",
+        site: "@goatslider",
+        title: "Goat Slider Documentation",
+        description: "A detailed documentation for Goat Slider.",
         images: [
-            {
-                url: '/assets/og_thumbnail.png'
-            }
-        ]
-      }
+          {
+            url: "/assets/og_thumbnail.png",
+          },
+        ],
+      },
     }
   },
   project: {
     link: "https://github.com/Azwedo/goatslider-docs",
   },
-  docsRepositoryBase: "https://github.com/Azwedo/goatslider-docs/blob/Production",
+  docsRepositoryBase:
+    "https://github.com/Azwedo/goatslider-docs/blob/Production",
   footer: {
-    text: "Goat Slider - A Project by Azwedo.com"
-  }
+    text: "Goat Slider - A Project by Azwedo.com",
+  },
 }
 
-export default config;
+export default config
